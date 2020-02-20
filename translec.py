04675c36d5f3bf917ee92f2998bf13a0b118c55b
anastaciopaulino <anastaciopaulino008@gmail.com>
2020-02-20 23:32:28 +0100

    I finished editing the Menu

--- translec.py
+++ translec.py
@@ -3,22 +3,7 @@ from googletrans import Translator
 import os
 
 translator = Translator()
-
-os.system('''
-white="\033[1;37m"                                          
-grey="\033[0;37m"                                           
-purple="\033[0;35m"                                         
-red="\033[1;31m"                                            
-green="\033[1;32m"                                          
-yellow="\033[1;33m"                                         
-purple="\033[0;35m"                                         
-cyan="\033[0;36m"                                           
-cafe="\033[0;33m"                                           
-fiuscha="\033[0;35m"                                        
-blue="\033[1;34m"                                           
-nc="\e[0m"                                                  
-
-''')
+count = 0
 
 class Trans:
 	def __init__(self, filename):
@@ -28,22 +13,20 @@ class Trans:
 		self.responso = open("pt-"+self.name,'w')
 
 		for line in self.send.readlines():   
-			self.traduzido = translator.translate(line.encode(), dest='pt').text
-			self.responso.write(self.traduzido.decode()+'\n')
+			self.traduzido = translator.translate(line, dest='pt').text
+			self.responso.write(self.traduzido+'\n')
+
 		# -*- Closing -*-
 		self.send.close()
 		self.responso.close()
 files = []
 class Path:
-    def __init__(self, path):
+    def __init__(self, path="."):
         self.path = os.listdir(path)
         self.dir = self.path
         for file in self.dir:
             if(os.path.isfile(file)):
-                if ((".srt" or ".vtt") in file):
+                if (".srt" in file) or (".vtt" in file):
                     files.append(file)
-                    print("ADD ===> ", file)
-Path(".")
-"""
-for file in files:
-	Trans(file)"""
+                    print(f" {count} ADD ===> ", file)
+Path()
